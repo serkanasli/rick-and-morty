c7fe176d532d9866dbd48011a2020c46fe672ace
Serkan Aslı <36230198+serkanasli@users.noreply.github.com>
2021-05-09 21:15:11 +0300

    commit

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import styles from "./header.module.css";
 
 function Header() {
--- components/section/search.js
+++ components/section/search.js
@@ -1,4 +1,4 @@
-import Image from "next/Image";
+import Image from "next/image";
 import styles from "./section.module.css";
 
 function Search() {
