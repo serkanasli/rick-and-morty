8de953d03479aaf963c1cf32360fd2b6ca87d133
Serkan Aslı <36230198+serkanasli@users.noreply.github.com>
2021-05-09 13:25:33 +0300

    home page done

--- components/section/showcase.js
+++ components/section/showcase.js
@@ -1,52 +1,18 @@
-import Image from "next/image";
-import Link from "next/link";
+import Card from "./card";
 import styles from "./section.module.css";
 
 function Showcase() {
   return (
     <div className={styles["showcase"]}>
-      <div className="container">
-        <div className={styles["character-card-wrapper"]}>
-          {["passive", "active", "active", "passive", "passive", "active"].map(
-            (value, index) => (
-              <article key={index} className={styles["character-card"]}>
-                <div className={styles["character-img"]}>
-                  <Image
-                    src="/img/rick-k22.jpeg"
-                    width={200}
-                    objectFit={"cover"}
-                    height={230}
-                  />
-                </div>
-                <div className={styles["character-content"]}>
-                  <section>
-                    <Link href="/about">
-                      <h2
-                        className={`button button-default ${styles["character-name"]}`}
-                      >
-                        Rick K-22
-                      </h2>
-                    </Link>
-                    <div className={styles["status"]}>
-                      <div className={styles[`status-icon-${value}`]}></div>
-                      <h5>Unknown - Alien</h5>
-                    </div>
-                  </section>
-                  <section>
-                    <p className={styles["character-short-detail"]}>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen...
-                    </p>
-                  </section>
-                </div>
-              </article>
-            )
-          )}
-        </div>
+      <div className={styles["pages-buttons"]}>
+        <button className="button button-outline-orange mr-05" type="button">
+          Geri
+        </button>
+        <button className="button button-outline-orange mr-05" type="button">
+          İleri
+        </button>
       </div>
+      <Card />
     </div>
   );
 }
